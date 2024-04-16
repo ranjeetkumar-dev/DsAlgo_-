ee5778918d3ddd69ddf14fec999abb327e9cc5db
ranjeet <ranjeetkumargond000@gmail.com>
2024-04-16 22:46:13 +0530

    max min element of an arr

--- 0_0#Reboot/arr.cpp
+++ 0_0#Reboot/arr.cpp
@@ -4,7 +4,7 @@ int main()
 {
     // arr declaration
     // initialise by 0 at start
-    int arr[10000] = {0};
+    int arr[10000] = {1};
 
     // int arr[8] = {1, 2, 3, 4, 5, 6, 7, 8};
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
